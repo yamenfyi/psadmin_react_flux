01a1d93e3379fec33bb5f4a147e9b0b1a1ce9ca6
yamenfyi <yamenfyi@gmail.com>
2018-12-26 10:11:25 -0500

    module-4 simple routing (after IFFY)

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -29164,37 +29164,42 @@ module.exports = Home;
 },{"react":157}],160:[function(require,module,exports){
 // We are deliberately not using "use strict" here because we need to define some global variables
 // like $ and jQuery -- this would cause a lint error that you can ignore.
+// We will implement below a workaround by putting all our code below, except the global defintions,
+// in an "IFFY" (Immiediatel-Invoked Function Expression) and put "use strict" within it -- no more lint error!
 
 $ = jQuery = require('jquery');
 var React = require('react');
 var Home = require('./components/homePage');
 var About = require('./components/about/aboutPage');
 
-var App = React.createClass({displayName: "App",
-    render: function() {
-        var Child;
+(function(win) {
+    "use strict";
 
-        switch(this.props.route) {
-            case 'about': Child = About; break;
-            default: Child = Home;
+    var App = React.createClass({displayName: "App",
+        render: function() {
+            var Child;
+    
+            switch(this.props.route) {
+                case 'about': Child = About; break;
+                default: Child = Home;
+            }
+    
+            return (
+                React.createElement("div", null, 
+                    React.createElement(Child, null)
+                )
+            );
         }
-
-        return (
-            React.createElement("div", null, 
-                React.createElement(Child, null)
-            )
-        );
+    });
+    
+    function render() {
+        var route = win.location.hash.substr(1);
+        React.render(React.createElement(App, {route: route}), document.getElementById('app'));
     }
-});
-
-function render() {
-    var route = window.location.hash.substr(1);
-    React.render(React.createElement(App, {route: route}), document.getElementById('app'));
-}
-
-window.addEventListener('hashchange', render);
-render();
-
-React.render(React.createElement(Home, null), document.getElementById('app'));
+    
+    win.addEventListener('hashchange', render);
+    render();
+    
+})(window);
 
 },{"./components/about/aboutPage":158,"./components/homePage":159,"jquery":1,"react":157}]},{},[160]);
--- src/main.js
+++ src/main.js
@@ -1,34 +1,39 @@
 // We are deliberately not using "use strict" here because we need to define some global variables
 // like $ and jQuery -- this would cause a lint error that you can ignore.
+// We will implement below a workaround by putting all our code below, except the global defintions,
+// in an "IFFY" (Immiediatel-Invoked Function Expression) and put "use strict" within it -- no more lint error!
 
 $ = jQuery = require('jquery');
 var React = require('react');
 var Home = require('./components/homePage');
 var About = require('./components/about/aboutPage');
 
-var App = React.createClass({
-    render: function() {
-        var Child;
+(function(win) {
+    "use strict";
 
-        switch(this.props.route) {
-            case 'about': Child = About; break;
-            default: Child = Home;
+    var App = React.createClass({
+        render: function() {
+            var Child;
+    
+            switch(this.props.route) {
+                case 'about': Child = About; break;
+                default: Child = Home;
+            }
+    
+            return (
+                <div>
+                    <Child />
+                </div>
+            );
         }
-
-        return (
-            <div>
-                <Child />
-            </div>
-        );
+    });
+    
+    function render() {
+        var route = win.location.hash.substr(1);
+        React.render(<App route={route} />, document.getElementById('app'));
     }
-});
-
-function render() {
-    var route = window.location.hash.substr(1);
-    React.render(<App route={route} />, document.getElementById('app'));
-}
-
-window.addEventListener('hashchange', render);
-render();
-
-React.render(<Home />, document.getElementById('app'));
+    
+    win.addEventListener('hashchange', render);
+    render();
+    
+})(window);
