ed8c5bbdda5fc5e1f4b4ee79bde90a6746f8a783
yamenfyi <yamenfyi@gmail.com>
2018-12-26 20:21:18 -0500

    module-7 switch back to hash history URL

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -44717,10 +44717,10 @@ var React = require('react');
 var Router = require('react-router');
 var routes = require('./routes');
 
-// The second optional parameter we are adding "Router.HistoryLocation" is for using
+// A second optional parameter we could add is "Router.HistoryLocation" which is for using
 // browser history URL (localhost:9000/about) instead of hash history URL (localhost:9000/#/about).
 // Note that the latter is the React default -- also has a wider browser support, but doesn't look nice!
-Router.run(routes, Router.HistoryLocation, function(Handler) {
+Router.run(routes, function(Handler) {
     React.render(React.createElement(Handler, null), document.getElementById('app'));
 });
 
--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ var React = require('react');
 var Router = require('react-router');
 var routes = require('./routes');
 
-// The second optional parameter we are adding "Router.HistoryLocation" is for using
+// A second optional parameter we could add is "Router.HistoryLocation" which is for using
 // browser history URL (localhost:9000/about) instead of hash history URL (localhost:9000/#/about).
 // Note that the latter is the React default -- also has a wider browser support, but doesn't look nice!
-Router.run(routes, Router.HistoryLocation, function(Handler) {
+Router.run(routes, function(Handler) {
     React.render(<Handler />, document.getElementById('app'));
 });
